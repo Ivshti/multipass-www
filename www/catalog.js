2e1f6e3e6656f0e126bf445f488f4c8963f75a0d
Ivo Georgiev <ivo@linvo.org>
2015-10-05 23:20:25 +0300

    Get items once, then only filter through them

--- www/catalog.js
+++ www/catalog.js
@@ -4,9 +4,18 @@ Catalog.config([ '$compileProvider', function($compileProvider) {
 	$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|stremio):/);
 }]);
 
-Catalog.factory('Items', [ '$q', function($q) {
+/*
+	Concept: 
+	- get the index of IMDB IDs against seeders, start with top
+	- get metadata form Cinemeta in chunks of 200, until we collect around 1000 - in memory?
+	- display them in Discover, 100 on a page, with category filter optional & filtering within that dataset
+*/
+
+Catalog.factory('Items', [ '$q', '$rootScope', function($q, $scope) {
 	var Client = require("stremio-addons").Client;
 	var addons = new Client();
+
+	var self = { };
 
 	addons.setAuth("http://api8.herokuapp.com","2a240788ce82492744cdd42ca434fc26848ec616");
 
@@ -14,37 +23,36 @@ Catalog.factory('Items', [ '$q', function($q) {
 	addons.add("http://stremio-cinemeta.herokuapp.com");
 	addons.add(window.location.origin); // multipass add-on
 
+	var genres = self.genres = {};
+	var res = [];
 	// NOTE: refresh sometimes
-	var popularities = { }
+	var popularities = { };
 	addons.call("stream.popularities", { }, function(err, res) {
-		if (res && res.popularities) popularities = res.popularities;
-	});
+		if (! (res && res.popularities)) return;
 
-	return {
-		get: function (opts, method) {
-			method = method || 'find';
-			var p = Object.keys(popularities)
-				.map(function(k) { return [k, popularities[k]] })
-				.sort(function(a,b) { return b[1] - a[1] }).map(function(x) { return x[0] });
-			console.log(p)
-			var options = $.extend(true, { query: { imdb_id: { $in: p } }, limit: 40, skip: 0, complete: true, popular: true,
-				sort: { popularity: -1 }
-			}, opts);
-			var deferred = $q.defer();
-			addons.meta[method](options,
-				function(err, items) {
-					if(err) deferred.reject(err); 
-					else deferred.resolve(items);
-				})
-			return deferred.promise;
-		},
-		popularities: function() {
-			return popularities;
-		}
-	};
+		popularities = res.popularities;
+
+		var p = Object.keys(popularities)
+			.map(function(k) { return [k, popularities[k]] })
+			.sort(function(a,b) { return b[1] - a[1] }).map(function(x) { return x[0] });
+		addons.meta.find({ query: { imdb_id: { $in: p } }, limit: 500, skip: 0, complete: true, popular: true, projection: "lean" }, function(err, r) {
+			if (!r) return;
+
+			res = r;
+			res.forEach(function(x) { 
+				if (! genres[x.type]) genres[x.type] = { };
+				x.genre.forEach(function(g) { genres[x.type][g] = 1 });
+			});
+			$scope.$apply();
+		});
+	});
+	self.all = function() { return res || [] };
+	self.popularities = function() { return popularities };
+
+	return self;
 }]);
 
-Catalog.controller('CatalogController', ['Items', '$timeout', '$window', '$q', function CatalogController(Items, $timeout, $window, $q) {
+Catalog.controller('CatalogController', ['Items', '$scope', '$timeout', '$window', '$q', function CatalogController(Items, $scope, $timeout, $window, $q) {
 	var self = this;
 
 	var imdb_proxy = '/poster/';
@@ -57,6 +65,16 @@ Catalog.controller('CatalogController', ['Items', '$timeout', '$window', '$q', f
 		movie: { name: 'Movies', genres: {} },
 		series: { name: 'TV Shows', genres: {} }
 	};
+	self.genres = Items.genres;
+
+	$scope.$watch(function() { return Items.loading }, function(loading) { self.loading = loading });
+	$scope.$watchCollection(function() {  return [self.showType, self.showGenre, Items.all().length] }, function() {
+		self.items = Items.all().filter(function(x) { 
+			return (x.type == self.showType) && 
+				(!self.showGenre || (x.genre.indexOf(self.showGenre) > -1))
+		});
+		console.log(self.items, Items.all().length)
+	});
 
 	self.formatImgURL = function formatImgURL(url, width, height) {
 		if (!url || -1 === url.indexOf("imdb.com")) return url;
@@ -69,47 +87,9 @@ Catalog.controller('CatalogController', ['Items', '$timeout', '$window', '$q', f
 	};
 
 
-	self.loadCatalog = function loadCatalog(more) {
-		if(self.loading || (self.query && more)) return false;
-
-		var parameters = {query: {type: self.showType}};
-
-		if(self.showGenre) {
-			parameters.query.genre = self.showGenre;
-		}
-		if(self.query) {
-			parameters.query = self.query;
-			// There's no infinite scroll here, so we pull a bit more results
-			parameters.limit = 50;
-		}
-		if(more) {
-			parameters.skip = self.movies.length;
-		} else {
-			self.movies = [];
-		}
-		self.loading = true;
-		return Items.get(parameters, self.query ? 'search' : 'find').then(function(result) {
-			var i;
-			if(self.query) {
-				result = result.results || [];
-				self.showType = 'search';
-			}
-			self.loading = false;
-			for(i = 0; i < result.length;i++) {
-				self.movies.push(result[i]);
-			}
-			if( ! more) {
-				self.selected = result[0];
-			}
-		}, function() {
-			self.loading = false;
-		});
-	};
-
 	self.selectGenre = function selectGenre(genre) {
 		if(self.showGenre === genre) return;
 		self.showGenre = genre;
-		self.loadCatalog();
 	};
 
 	self.selectType = function selectType(type) {
@@ -117,7 +97,6 @@ Catalog.controller('CatalogController', ['Items', '$timeout', '$window', '$q', f
 		self.showType = type;
 		self.query = '';
 		self.showGenre = '';
-		self.loadCatalog();
 	};
 
 	self.selectMovie = function selectMovie(movie) {
@@ -127,7 +106,6 @@ Catalog.controller('CatalogController', ['Items', '$timeout', '$window', '$q', f
 	self.startSearch = function startSearch() {
 		self.query = self.query.trim();
 		self.movies = [];
-		self.loadCatalog();
 	};
 
 	self.playMovie = function playMovie(movie) {
@@ -144,18 +122,7 @@ Catalog.controller('CatalogController', ['Items', '$timeout', '$window', '$q', f
 	self.downloadLink = function(banner) {
 		// TODO
 	};
-	self.loadCatalog();
 
-	// A single query to load the movies and series categories
-	Items.get({limit: 600, query: {}, popular: true, projection:{ type: 1, genre:1 } }).then(function(result) {
-		result.forEach(function(res) {
-			var type = res.type || 'movie';
-			res.genre && res.genre.forEach(function(genre) {
-				self.catTypes[type].genres[genre] = 1;
-			});
-		});
-	});
 	return self;
-
 }]); 
 
